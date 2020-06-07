2ebd0387fbdbd6728569c1399aad58e0241d7834
Andrey Shtamburg <andrewshtamburg@gmail.com>
2020-06-07 13:15:47 +0200

    Updated initial db script

--- init_db_script.sql
+++ init_db_script.sql
@@ -1,11 +1,26 @@
-te table sources (
+create table users (
+    "id"            serial,
+    "admin"         boolean not null default false,
+    "email"         varchar(64) not null unique,
+    "password_hash" varchar(128),
+    "first_name"    varchar(64) not null,
+    "last_name"     varchar(64) not null,
+    "uuid"          varchar(128) not null,
+    "created_at"    timestamp not null default current_timestamp,
+    "last_updated"  timestamp not null default current_timestamp,
+    constraint user_id_pkey primary key (id)
+);
+
+create table sources (
     "id"           serial,
     "name"         varchar(128) not null,
     "description"  varchar(1024),
     "link"         varchar(256),
     "created_at"   timestamp   not null default current_timestamp,
     "last_updated" timestamp   not null default current_timestamp,
-    constraint source_id_pkey primary key (id)
+    "user_id"      integer,
+    constraint source_id_pkey primary key (id),
+    constraint sources_user foreign key (user_id) references users (id) on delete cascade
 );
 
 create table tags (
@@ -22,6 +37,22 @@ create table source_tag (
     constraint source_tag_tags foreign key (tag_id) references tags (id) on delete cascade
 );
 
+create table blacklist_tokens (
+    "id"            serial,
+    "token"         varchar(500) not null unique,
+    "blacklisted_on" timestamp  not null,
+    constraint blacklist_id_pkey primary key (id)
+);
+
+insert into users (admin, email, first_name, last_name, uuid, password_hash)
+values
+(true, 
+'andrewshtamburg@gmail.com', 
+'Andrei', 
+'Shtamburg', 
+'6295de0e-0584-404c-a165-5bd642a3188f',
+'5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8');
+
 insert into tags (name) 
 values 
 ('python'),
@@ -34,9 +65,9 @@ values
 ('react'),
 ('full stack');
 
-insert into sources (name, description, link, created_at, last_updated)
+insert into sources (name, description, link, created_at, last_updated, user_id)
 values ('CRUD app with mongo and flask', 'Okta developer explains how to create a simple CRUD app using flask, mongodb, docker-compose', 
-'https://developer.okta.com/blog/2018/12/20/crud-app-with-python-flask-react', NOW()::timestamp, NOW()::timestamp);
+'https://developer.okta.com/blog/2018/12/20/crud-app-with-python-flask-react', NOW()::timestamp, NOW()::timestamp, 1);
 
 insert into source_tag (source_id, tag_id)
 values (1, 9),
